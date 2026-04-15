700e6e94d6b81656ef62a0d06f31f47c5c670a86
ShonTechDev <shonta.pierce@gmail.com>
2026-04-14 21:58:24 -0400

    created components (TodoList, TodoForm) and moved into componenets and updated app.jsx structure,moved into separate files

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import './App.css'
+import TodoList from './TodoList.jsx';
+import TodoForm from './TodoForm.jsx';
 
 function App() {
-
-const todoList = [
-  {id: 1, title: "populate flashcards"},
-  {id: 2, title: "study vocabulary"},
-  {id: 3, title: "actively code to solidify material"},
-]
-
   return (
     <div>
-      <h1>My Todos</h1>
-      <ul>
-        {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
-      </ul>
+      <h1>Todo List</h1>
+      <TodoForm />
+      <TodoList />
     </div>
   );
 }
