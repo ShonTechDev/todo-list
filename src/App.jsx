2f89b332ec918b7eefc5275f9332700856517929
ShonTechDev <shonta.pierce@gmail.com>
2026-04-26 18:21:26 -0400

    Lesson 4- added todo form with event handling and useRef

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import './App.css'
 import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
 
-const todos = [
-  {id: 1, title: "populate flashcards"},
-  {id: 2, title: "study vocabulary"},
-  {id: 3, title: "actively code to solidify material"},
-];
-
 function App() {
-  const [todoList, setTodoList] = useState(todos);
+  const [todoList, setTodoList] = useState([]);
 
+function addTodo(todoTitle) { //receives new todo text and adds it into todoList state
+    const newTodo = {
+      id: Date.now(), //generates a unique ID for each todo
+      title: todoTitle,
+    };
+    setTodoList((previous) => [newTodo, ...previous]) //puts the new to do first, maintains the old ones
+}
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoForm />
+      <TodoForm onAddTodo={addTodo} /> {/*passes the handler to TodoForm */}
       <TodoList todoList={todoList} />
     </div>
   );
